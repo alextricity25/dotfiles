6db9405755b1ad7e8fc3f8e48e430763f6c330a0
Miguel Alex Cantu <miguel.can2@gmail.com>
2021-08-01 20:23:46 -0500

    Added keymappings for ToggleTerm

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -2,3 +2,5 @@ local utils = require('utils')
 
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 utils.map('i', 'jk', '<Esc>')           -- jk to escape
+utils.map('i', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
+utils.map('n', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
